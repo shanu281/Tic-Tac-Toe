90f301a11724761ab4290f7f11ac4b350c3330ff
Shanu <shanukumar@shanus-MacBook-Air.local>
2022-11-24 16:32:20 +0530

    project completed

--- src/components/Board/index.jsx
+++ src/components/Board/index.jsx
@@ -5,19 +5,60 @@ class Board extends react.Component {
         super(props)
         this.state = {
             currentPlayer: 'X',
-            boxes: ["", "", "", "", "", "", "", "", ""]
+            boxes: ["", "", "", "", "", "", "", "", ""],
+            history: [],
+            winner: "",
         }
     }
 
     handleClick = (i) => {
+        if (this.state.boxes[i] || this.state.winner) {
+            return
+        }
         let newBoxes = [...this.state.boxes]
         newBoxes[i] = this.state.currentPlayer
+        let newWinner = this.calculateWinner(newBoxes)
         this.setState({
             currentPlayer: this.state.currentPlayer === "X" ? "O" : "X",
-            boxes: newBoxes
+            boxes: newBoxes,
+            winner: newWinner,
+            history: this.state.history.concat([newBoxes])
+        })
+        console.log(newBoxes)
+        console.log(newWinner)
+        console.log(this.state.history)
+    }
+    handleReset = () => {
+        this.setState({
+            currentPlayer: "X",
+            boxes: ["", "", "", "", "", "", "", "", ""],
+            history: []
         })
     }
-
+    handleStep = (i) => {
+        this.setState({
+            boxes: this.state.history[i]
+        })
+    }
+    calculateWinner(squares) {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
+    }
     render() {
         return (
             <>
@@ -29,9 +70,16 @@ class Board extends react.Component {
                                 <div onClick={() => this.handleClick(i)} className="box">{box}</div>
                             )
                         }
+                        <button onClick={this.handleReset}>Reset </button>
                     </div>
                     <div className="board-details">
-                        <h2  >Next Player : {this.state.currentPlayer}</h2>
+                        <h2 className={this.state.winner === null || this.state.winner === "" ? "show" : "hide"} >Next Player : {this.state.currentPlayer}</h2>
+                        <h1 className={this.state.winner === null || this.state.winner === "" ? "hide" : "show"}>Winner is : {this.state.winner}</h1>
+                        {
+                            this.state.history.map((elm, i) =>
+                                <button onClick={() => this.handleStep(i)} className="sec-btn">Go to step {i + 1}</button>
+                            )
+                        }
                     </div>
                 </div>
             </>
